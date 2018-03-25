5ea0f22aa538f65874e922d0b0bc7c1bc1942460
Sandeep <urssandy84@gmail.com>
2018-03-25 09:11:29 +0530

    Added an abstract adapter class

--- fit-service/src/main/java/com/gefa/fit/boundary/inbound/jms/AssetEventAdapter.java
+++ fit-service/src/main/java/com/gefa/fit/boundary/inbound/jms/AssetEventAdapter.java
@@ -1,32 +1,9 @@
 package com.gefa.fit.boundary.inbound.jms;
 
-import com.gefa.fit.application.AssetService;
 import com.gefa.fit.boundary.inbound.jms.events.AssetEvent;
-import com.gefa.fit.boundary.inbound.jms.events.converter.AssetEventConverter;
 
-import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
+public abstract class AssetEventAdapter {
 
-@ApplicationScoped
-public class AssetEventAdapter {
-
-	@Inject
-	private AssetService assetService;
-
-	@Inject
-	private AssetEventConverter assetEventConverter;
-
-	public void handleEvent(AssetEvent assetEvent) {
-
-		String eventType = assetEvent.getEventType();
-		switch (eventType){
-			case "asset-created":
-				assetService.create(assetEventConverter.toAsset(assetEvent));
-				break;
-			default:
-
-		}
-
-	}
+	public abstract void handleEvent(AssetEvent assetEvent) ;
 
 }
